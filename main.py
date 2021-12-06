bda99ff59f2030805526a0e47b4765b3ff05f111
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-12-06 20:32:46 +0500

    Harris Corner detector and FAST detector added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 # path to the video
 video_path = 'C:/Habib University/OneDrive - Habib University/University/7th Semester/Digital Image Processing/Project/FD in Dynamic Environment/Dynamic1.mp4'
@@ -13,6 +13,10 @@ frameNr = 0
 total_kp_sift = 0
 # total keypoints in the video via ORB
 total_kp_orb = 0
+# total keypoints in the video via Harris cornor detector
+total_kp_hcd = 0
+# total keypoints in the video via FAST
+total_kp_fast = 0
 
 while(True):
 
@@ -35,13 +39,26 @@ while(True):
         # find the keypoints with ORB
         kp_orb = orb.detect(gray,None)
 
+        # find the keypoints using Harris cornor detector
+        gray_hcd = np.float32(gray)
+        kp_hcd = cv2.cornerHarris(gray_hcd,2,3,0.04)
+
+        # Initiate FAST object with default values
+        fast = cv2.FastFeatureDetector_create()
+        # find and draw the keypoints
+        kp_fast = fast.detect(gray,None)
+
         total_kp_sift += len(kp_sift)
         total_kp_orb += len(kp_orb)
+        total_kp_hcd += len(kp_hcd)
+        total_kp_fast += len(kp_fast)
 
         print('Dimensions of Frame ', str(frameNr),frame.shape)
         print('SIFT Keypoints in Frame ', str(frameNr) , ' length of keypoints ', len(kp_sift))
         print('ORB Keypoints in Frame ', str(frameNr) , ' length of keypoints ', len(kp_orb))
-        
+        print('HCD Keypoints in Frame ', str(frameNr) , ' length of keypoints ', len(kp_hcd))
+        print('FAST Keypoints in Frame ', str(frameNr) , ' length of keypoints ', len(kp_fast))
+
     else:
         break # exist the loop when frames are finished
  
@@ -51,3 +68,5 @@ print(" = "*12)
 print('\n'*5)
 print('Total SIFT keypoints in the video', total_kp_sift)
 print('Total ORB keypoints in the video', total_kp_orb)
+print('Total HCD keypoints in the video', total_kp_hcd)
+print('Total FAST keypoints in the video', total_kp_fast)
