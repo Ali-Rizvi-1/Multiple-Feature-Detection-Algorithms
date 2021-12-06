0b6f0aab4186502020891fa184006f417e67957d
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-12-06 20:26:02 +0500

    SIFT detection

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ capture = cv2.VideoCapture(video_path)
 # initial the frame number of the video frames 
 frameNr = 0
 
+# total keypoints in the video via SIFT
+total_kp_sift = 0
+
 while(True):
 
     flag, frame = capture.read()
@@ -17,8 +20,24 @@ while(True):
 
         print(frameNr)
 
+        # convert image to gray scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Initiate SIFT detector
+        sift = cv2.SIFT_create()
+        # find the keypoints with SIFT
+        kp_sift = sift.detect(gray,None)
+
+        total_kp_sift += len(kp_sift)
+
+        print('Dimensions of Frame ', str(frameNr),frame.shape)
+        print('SIFT Keypoints in Frame ', str(frameNr) , ' length of keypoints ', len(kp_sift))
+        
     else:
         break # exist the loop when frames are finished
  
     frameNr = frameNr+1 # increment in frame number
 
+print(" = "*12)
+print('\n'*5)
+print('Total SIFT keypoints in the video', total_kp_sift)
